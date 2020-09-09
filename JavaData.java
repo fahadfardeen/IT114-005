e2d89d0ea497b849ef94f80baf0e05206425aa3f
fahadfardeen <aff9@njit.edu>
2020-09-09 14:17:46 -0400

    Adding JavaData Homework

---
+++ JavaData.java
@@ -0,0 +1,14 @@
+public class JavaData {
+	public static void main(String[] args) {
+
+		String name = "Fahad";
+		int age = 22;
+		float decimal = 15.5f;
+
+		System.out.println("Hey Professor, my name is " + name);
+
+		System.out.println("I am " + age);
+
+		System.out.println("And this is a decimal " + decimal);
+	}
+}
