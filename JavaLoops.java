fb7b4cf31084bea92037317d5eaae0fdcdd2e8af
fahadfardeen <aff9@njit.edu>
2020-09-16 14:35:31 -0400

    Added JavaLoops homework

---
+++ JavaLoops.java
@@ -0,0 +1,15 @@
+public class JavaLoops {
+	public static void main(String[] args) {
+
+		int a = 10;
+
+		System.out.println("Even numbers between 1 and 10 are");
+
+		for (int i = 1; i <= a; i++) {
+			if (i % 2 == 0) {
+				System.out.println(i);
+			}
+
+		}
+	}
+}
